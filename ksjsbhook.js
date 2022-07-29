ad7eeec8cf3e8b3f394600a10694d2ac088c98a1
FuShengPing <103162025+FuShengPing@users.noreply.github.com>
2022-07-30 00:07:10 +0800

    更新

--- ksjsbhook.js
+++ ksjsbhook.js
@@ -1,15 +1,19 @@
+
 function getSig3(str){
-    var testStr = "12345678"
+
     var sig3 = ''
     Java.perform(
         function(){
             var IKSecurityExCls = Java.use("com.kuaishou.android.security.KSecurity");
-            sig3 = IKSecurityExCls.atlasSign(testStr)
+            sig3 = IKSecurityExCls.atlasSign(str)
         }
     )
-    console.log("sig3:",sig3)
-    console.log("sig3Length:",sig3.length)
+    
     return sig3;
 }
 
-setTimeout(getSig3)
+rpc.export = {
+    getSig3: function(str){
+        return getSig3(str);
+    }
+}
--- ksjsbhook.py
+++ ksjsbhook.py
@@ -7,12 +7,13 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def getsig():
     str = request.args.get('str', '')
+    print("param：",str)
     session = frida.get_device_manager().get_remote_device().attach('快手极速版')
-    with codecs.open('./hookzr.js', 'r', 'utf-8') as f:
+    with codecs.open('./ksjsbhook.js', 'r', 'utf-8') as f:
         source = f.read()
     #return source
     script = session.create_script(source)
     script.load()
     sign = script.exports.getsig(str)
     return sign
-app.run(port=5000)
+app.run("0.0.0.0",port=5000)
